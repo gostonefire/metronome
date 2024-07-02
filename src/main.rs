3426071327c080c794c9555d6227fbb4058260bd
gostonefire <peter.steneld@gmail.com>
2024-07-02 16:25:42 +0200

    Adds cli

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,43 @@ use rodio;
 use rodio::{OutputStream, Sink};
 use tokio::time::Instant;
 use sound::Sound;
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Starting tempo
+    #[arg(short, long, default_value_t = 60)]
+    start: u8,
+
+    /// End goal tempo
+    #[arg(short, long, default_value_t = 180)]
+    end: u8,
+
+    /// Increase step
+    #[arg(short, long, default_value_t = 2)]
+    increase: u8,
+
+    /// Decrease step
+    #[arg(short, long, default_value_t = 1)]
+    decrease: u8,
+
+    /// Length of play segment
+    #[arg(short, long, default_value_t = 10)]
+    length: u8,
+}
 
 #[tokio::main]
 async fn main() {
-    let mut tempo: f64 = 60.0;
-    let segment: i64 = 5;
-    let mut incrementor: f64 = -1.0;
+    let args = Args::parse();
+
+    let mut tempo: f64 = args.start as f64;
+    let end_tempo: f64 = args.end as f64;
+    let increase: f64 = args.increase as f64;
+    let decrease: f64 = args.decrease as f64 * -1.0;
+    let segment: i64 = args.length as i64;
+
+    let mut incrementor: f64 = decrease;
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
@@ -23,7 +54,7 @@ async fn main() {
         let delay = Duration::from_millis((60000.0 / tempo) as u64);
         let mut interval = tokio::time::interval_at(start + delay, delay);
 
-        for n in 0..segment {
+        for _ in 0..segment {
             tokio::select! {
                 _ = interval.tick() => {
                     start = Instant::now();
@@ -32,7 +63,10 @@ async fn main() {
                 }
             }
         }
-        incrementor = if incrementor < 0.0 {2.0} else {-1.0};
+        if tempo >= end_tempo {
+            break;
+        }
+        incrementor = if incrementor < 0.0 {increase} else {decrease};
         tempo += incrementor;
     }
 }
