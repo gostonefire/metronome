91a3599b0c585779188475fd2c73b7c82d08afd9
gostonefire <peter.steneld@gmail.com>
2024-07-03 12:29:14 +0200

    removes tokio

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod sound;
 use std::time::Duration;
 use rodio;
 use rodio::{OutputStream, Sink};
-use tokio::time::Instant;
 use sound::Sound;
 use clap::{Parser};
 
@@ -31,37 +30,36 @@ struct Args {
     length: u8,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let args = Args::parse();
 
-    let mut tempo: f64 = args.start as f64;
+    let tempo: f64 = args.start as f64;
     let end_tempo: f64 = args.end as f64;
     let increase: f64 = args.increase as f64;
     let decrease: f64 = args.decrease as f64 * -1.0;
     let segment: i64 = args.length as i64;
 
+    metronome_b(tempo, end_tempo, increase, decrease, segment);
+
+}
+
+fn metronome_b(mut tempo: f64, end_tempo: f64, increase: f64, decrease: f64, segment: i64) {
     let mut incrementor: f64 = decrease;
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
-    //let sound = Sound::load("tracks/UHH_AcoustiHatB_Hit-02_trunc.wav").unwrap();
     let sound = Sound::get().unwrap();
 
-    let mut start = Instant::now();
+    let mut start = std::time::Instant::now();
     loop {
         println!("Tempo: {}", tempo);
         let delay = Duration::from_millis((60000.0 / tempo) as u64);
-        let mut interval = tokio::time::interval_at(start + delay, delay);
 
         for _ in 0..segment {
-            tokio::select! {
-                _ = interval.tick() => {
-                    start = Instant::now();
-                    sink.append(sound.decoder());
-                    sink.sleep_until_end();
-                }
-            }
+            std::thread::sleep(delay - (std::time::Instant::now() - start));
+            start = std::time::Instant::now();
+            sink.append(sound.decoder());
+            sink.sleep_until_end();
         }
         if tempo >= end_tempo {
             break;
